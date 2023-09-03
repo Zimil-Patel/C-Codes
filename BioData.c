bc69b65891668f0642ceae6c9adb3c05c195c0ae
Zimil Patel <112332000+Zimil-Patel@users.noreply.github.com>
2023-09-03 14:19:58 +0530

    BioData.c
    03-09-2023

--- BioData.c
+++ BioData.c
@@ -2,45 +2,49 @@
 
 int main(){
 
-	char name, temp, languageSkilled;
-	unsigned short int age, mark;
+	// char name, temp, languageSkilled;
+	unsigned short int age, markOfSSC;
 	unsigned int pinCode;
 	float percentage;
 	
-	//Name
-	printf("Enter your name :");
-	scanf("%c",&name);
+	// //Name
+	// printf("Enter your name :");
+	// scanf("%c",&name);
 	
-	//Age
+	// //Age
 	printf("Enter your age :");
 	scanf("%hu",&age);
 	
-	//10th mark
+	// //10th mark
 	printf("Enter marks obtained in 10th :");
-	scanf("%hu",&mark);
-	
-	//area pin code
-	printf("Enter area pin code :");
-	scanf("%u",&pinCode);
-	
-	//10th percentage
+	scanf("%hu",&markOfSSC);
+
+	// //10th percentage
 	printf("Enter percentage of 10th :");
 	scanf("%f",&percentage);
 	
-	//languageSkilled
-	printf("Enter language you have skilled :");
-	scanf(" %c",&languageSkilled);
+	// //area pin code
+	printf("Enter area pin code :");
+	scanf("%u",&pinCode);
+	
+	
+	// //languageSkilled
+	// printf("Enter language you have skilled :");
+	// scanf(" %c",&languageSkilled);
 	
 
 	
 	//Bio data
 	printf("\n--- Prepared Bio Data ---");
-	printf("\nName : %c",name);
+	printf("\nFirst Name : Zimil");
+	printf("\nLast Name : Patel");
+	printf("\nDate of Birth : 10-01-2003");
 	printf("\nAge : %hu",age);
-	printf("\n10th Mark : %hu",mark);
-	printf("\nArea pin code : %u",pinCode);
+	printf("\n10th Mark : %hu",markOfSSC);
 	printf("\n10th percentage : %f",percentage);
-	printf("\nSkilled language : %c",languageSkilled);
+	printf("\nExams cleared : 10th, 12th, BCA");
+	printf("\nArea pin code : %u",pinCode);
+	printf("\nSkilled languages : c, java);
 	
 	return 0;
 	
