4e61149591a3310fbd6e7aa2a57c1ec1dcb3fcf6
Zimil Patel <112332000+Zimil-Patel@users.noreply.github.com>
2023-12-02 18:45:48 +0530

    findCube.c

--- UDFTasks/findCube.c
+++ UDFTasks/findCube.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-//Q.1 Write a Program to find the cube of a given number using UDF
+//Q.1 Write a Program to find the cube of a given number using UDF!
 void findCube(){
 
 	int num;
